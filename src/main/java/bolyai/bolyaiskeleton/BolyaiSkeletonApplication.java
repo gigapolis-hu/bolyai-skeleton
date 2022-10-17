8dc45c9515ecd5b92ea052209ebafbaf4c697bfc
Balázs Mátyássy <balazs.matyassy@hotmail.com>
2022-10-17 16:18:51 +0200

    Initial commit

---
+++ src/main/java/bolyai/bolyaiskeleton/BolyaiSkeletonApplication.java
@@ -0,0 +1,13 @@
+package bolyai.bolyaiskeleton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BolyaiSkeletonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BolyaiSkeletonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/bolyai/bolyaiskeleton/BolyaiSkeletonApplicationTests.java
@@ -0,0 +1,13 @@
+package bolyai.bolyaiskeleton;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BolyaiSkeletonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
